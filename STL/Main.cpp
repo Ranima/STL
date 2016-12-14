d8975c517a8c171979fc197d8e551f71ae7f3479
Ranima <raphaelnmastel3@gmail.com>
2016-12-13 17:56:08 -0800

    more prep

--- STL/Main.cpp
+++ STL/Main.cpp
@@ -2,6 +2,16 @@
 
 int main()
 {
-
+	std::vector<int> vectorOfInts;
+	vectorOfInts.push_back(5);
+	vectorOfInts.push_back(2);
+	vectorOfInts.push_back(7);
+	int secondValue = vectorOfInts.at(1);
+	int thirdValue = vectorOfInts[2];
+	vectorOfInts[2] = 10;
+	if (vectorOfInts.size() > 2)
+	{
+		vectorOfInts[2] = 10;
+	}
 	return 0;
 }
